c47c2e184e812c4fc6e49192506994947121557b
pranjali kotgire <pranjalikotgire57@gmail.com>
2020-11-15 15:46:08 -0800

    Movies project Initial code

---
+++ routes/movies.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const http = require('http');
+const https = require('https');
+const dotenv = require('dotenv');
+
+// var Client = require('node-rest-client').Client;
+// var client = new Client();
+const API_KEY = process.env.MOVIESDB_API_KEY;
+console.log('API_KEY: ' + API_KEY);
+const MovieDB = require('moviedb')(API_KEY);
+const fetch = require('node-fetch');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  var movieResponse;
+  MovieDB.movieInfo({ id: 666 }, (err, res) => {
+    movieResponse = res;
+    console.log(res);
+  });
+  res.send('movies data');
+});
+
+module.exports = router;
